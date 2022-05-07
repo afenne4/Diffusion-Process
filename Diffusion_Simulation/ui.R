52f53e72ed10ee21bdaf9460367157bbd08c533a
afenne4 <afenne4@gmail.com>
2022-05-07 17:17:24 -0400

    Set up page with tabs and UI for simulated diffusion model parameters

--- Diffusion_Simulation/ui.R
+++ Diffusion_Simulation/ui.R
@@ -3,38 +3,39 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(navbarPage(title = "The Diffusion Model?",
+                   tabPanel("What is The Diffusion Model",
+                            h4("This App creates an interactive widget to demonstrate
+                               how altering diffusion models paramters influences 
+                               the diffusion process")
+                            ),
+                   tabPanel("Simulated Diffusion Path",
+                            sidebarLayout(
+                                sidebarPanel(
+                                    h3("Select Values for Diffusion Model Parameters"),
+                                    sliderInput("V",
+                                                "Select Drift Rate:",
+                                                min = -.5,
+                                                max = .5,
+                                                value = 0),
+                                    sliderInput("a",
+                                                "Select Boundary Separation:",
+                                                min=5,
+                                                max=15,
+                                                value=10),
+                                    sliderInput("z",
+                                                "Select Starting Point:",
+                                                min=-3,
+                                                max=3,
+                                                value=0),
+
+                                ),
+                                mainPanel(
+                                    h2("Gaussian Distribution"),
+                                    plotOutput("distPlot")
+                                ))
+                   
     
-    # Application title
-    titlePanel("Random Numbers"),
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            h3("Select number of bins, mean of distribution and SD"),
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            sliderInput("mu",
-                        "Select mean of distribution:",
-                        min=-5,
-                        max=5,
-                        value=0),
-            sliderInput("SD",
-                        "Select standard deviation:",
-                        min=0.1,
-                        max=5,
-                        value=1),
-            checkboxInput("ShowTitle","Show/Hide Title")
-            
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            h2("Gaussian Distribution"),
-            plotOutput("distPlot")
-        )
+
     )
 ))
